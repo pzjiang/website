b37cdb31d0ae766f1fccc82999beff04bd9dc2ce
pzjiang <pzjiang01@hotmail.com>
2021-01-10 19:23:23 -0500

    added the links for the banner

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,30 @@
 import React from "react"
 import layoutstyle from "../styles/layoutstyle.module.css"
+import { Link } from "gatsby"
 
+
+const ListLink = props => (
+    <li className={layoutstyle.tabs}>
+        <Link to={props.to}>{props.children}</Link>
+    </li>
+)
 
 export default function Layout({ children }) {
     return (
 
         <div className={layoutstyle.banner}>
-            <h1 className={layoutstyle.banned}>Hello</h1>
+            <header className={layoutstyle.banned}>
+                <h1>Hello</h1>
+                <ul className={layoutstyle.pagetabs}>
+                    <ListLink to="/">Home</ListLink>
+                    <ListLink to="/contact/">Contact</ListLink>
+
+
+                </ul>
+
+
+            </header>
+
 
             {children}
 
---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout.js"
+
+export default function Contact() {
+    return (
+        <Layout>
+            <h1>goodbye world</h1>
+
+
+        </Layout>
+    )
+
+}
