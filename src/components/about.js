f68cc69bd0fe8b8f003c2afc792f87732b5f15b2
pzjiang <pzjiang01@hotmail.com>
2021-01-15 16:43:47 -0500

    making it a one page with anchor links in the tab list

---
+++ src/components/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+import style from "../styles/aboutstyles.module.css";
+
+
+export default function About() {
+
+    return (
+        <section className={style.sect}>
+
+
+
+
+        </section>
+
+    )
+
+}
